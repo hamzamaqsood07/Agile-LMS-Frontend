45657f4421eb9bbab9e4576b4570054f6e881c16
zubairmaqsood <149279927+zubairmaqsood@users.noreply.github.com>
2024-09-20 21:31:21 +0500

    change css of mainbar component and apply texfields to filter the students according to given filter

--- src/routes/Table.jsx
+++ src/routes/Table.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import { useState } from 'react';
+import TextField from '@mui/material/TextField';
 
 import { AgGridReact } from 'ag-grid-react';
-import 'ag-grid-community/styles/ag-grid.css'; // Core grid CSS
+import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-alpine.css';
 import { useLoaderData } from 'react-router-dom';
 export default function StudentTable() {
-  
+
   const students = useLoaderData()
 
   const dateFormatter = params => {
     const date = new Date(params.value);
-    return date.toLocaleDateString(); 
+    return date.toLocaleDateString();
   };
 
   const [columnDefs] = useState([
@@ -27,16 +28,64 @@ export default function StudentTable() {
     { headerName: "Updated Date", field: "updatedDate", sortable: true, filter: true, valueFormatter: dateFormatter },
     { headerName: "Status Date", field: "statusDate", sortable: true, filter: true, valueFormatter: dateFormatter }
   ]);
-  
+
 
   const [rowData] = useState(students);
-  
+
   return (
-    <div  className="ag-theme-alpine" style={{ height: '400px', width: '100%' }} >
-       <AgGridReact
-        columnDefs={columnDefs}
-        rowData={rowData}
-      />
-    </div>
+    <>
+      <div className='studentTableSearchBar'>
+
+        <TextField
+          sx={{ position: "absolute", top: "24%", left: "2%", width: "15%" }}
+          name='searchByName'
+          placeholder="Search by name"
+          variant="outlined"
+          size='small'
+        />
+
+        <TextField
+          sx={{ position: "absolute", top: "24%", left: "20%", width: "15%" }}
+          name='searchByFamily'
+          placeholder="Search by family"
+          variant="outlined"
+          size='small'
+        />
+
+        <TextField
+          sx={{ position: "absolute", top: "24%", left: "39%", width: "16%" }}
+          name='searchByStatus'
+          select
+          label="Search by status"
+          variant="outlined"
+          size='small'
+        />
+
+        <TextField
+          sx={{ position: "absolute", top: "24%", left: "58%", width: "16%" }}
+          name='searchByClass'
+          select
+          label="Search by class"
+          variant="outlined"
+          size='small'
+        />
+
+        <TextField
+          sx={{ position: "absolute", top: "24%", left: "77%", width: "16%" }}
+          name='searchBySection'
+          select
+          label="Search by section"
+          variant="outlined"
+          size='small'
+        />
+      </div>
+
+      <div className="ag-theme-alpine" style={{ height: '80%', width: '100%', position: "absolute", top: "20%", left: "0%"}} >
+        <AgGridReact
+          columnDefs={columnDefs}
+          rowData={rowData}
+        />
+      </div>
+    </>
   )
 }
